fcd6ed85f5ffcd9c457d81c5c206bf5e0af19882
Michele Mancusi <demancum@destc0strapp14>
2024-08-14 13:14:57 +0200

    Add save_config_callback=None to main

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from contrastive_model.contrastive_model import CoCola
 
 
 def cli_main():
-    cli = LightningCLI(CoCola, CoColaDataModule) #save_config_callback=None
+    cli = LightningCLI(CoCola, CoColaDataModule, save_config_callback=None) 
 
 
 if __name__ == "__main__":
