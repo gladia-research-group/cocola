cb58c977f7c9b18e82d35093ef269f9bde6a65f3
Michele <michele.mancusi@hotmail.it>
2024-08-29 10:00:34 +0200

    Add requirement that anchor and positive should be of size 2 both

--- data/musdb_contrastive_preprocessed.py
+++ data/musdb_contrastive_preprocessed.py
@@ -131,7 +131,7 @@ class MusdbContrastivePreprocessed(Dataset):
 
         for track in tqdm(tracks, desc="Preprocessing tracks"):
             if self.test == "pitch_shift":
-                stems_paths = [path for path in track.glob("*.wav") if path.name != "mixture.wav" and path.name != "drums.wav"]
+                stems_paths = [path for path in track.glob("*.wav") if path.name != "mixture.wav"]
             else:
                 stems_paths = [path for path in sorted(track.glob("*.wav")) if path.name != "mixture.wav"]
 
@@ -152,14 +152,12 @@ class MusdbContrastivePreprocessed(Dataset):
                     stems_idxs = [i for i in range(len(stems)) if i != 1]
 
                 for i in range(len(stems[0])):
-                    anchor_mix_size = random.randint(
-                        1, len(stems_idxs) - 1) if self.generate_submixtures else 1
+                    anchor_mix_size = 2
                     anchor_mix_idxs = random.sample(
                         stems_idxs, anchor_mix_size)
                     
                     if self.test == "pitch_shift":
-                        positive_mix_size = random.randint(
-                            1, len(stems_idxs) - len(anchor_mix_idxs)) if self.generate_submixtures else 1
+                        positive_mix_size = 2
                         positive_mix_idxs = random.sample(
                             [stem_idx for stem_idx in stems_idxs if stem_idx not in anchor_mix_idxs], positive_mix_size)
                     else:
